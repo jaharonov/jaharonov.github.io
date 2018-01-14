da1a024d8dd4d599c3824902131fda3a9bd85c39
jaharonov <jaharonov@users.noreply.github.com>
2018-01-14 14:40:10 -0600

    Update client.js

--- portfolio-site/server/public/scripts/client.js
+++ portfolio-site/server/public/scripts/client.js
@@ -19,7 +19,7 @@ $mdThemingProvider.theme('default')
 
     $routeProvider
         .when('/landing', {
-            templateUrl: '../public/views/landing.html',
+            templateUrl: 'public/views/landing.html',
             controller: 'MaterialController as mc',
         }).otherwise({
             redirectTo: 'landing'
